98156d139d07d7901ddbbb072de1a19be8b67fdd
georgestamatelis <52538570+georgestamatelis@users.noreply.github.com>
2019-12-23 14:03:54 +0200

    Add files via upload

--- oopProj/cards/item.h
+++ oopProj/cards/item.h
@@ -6,5 +6,7 @@ class Item:public greenCard
 private:
   int durability;
 public:
-
+  Item(string name,int cost,int AB,int DB,int mh,int eb,int ec,int durability,string t="item")
+  :greenCard(name,cost,AB,DB,mh,eb,ec,t),durability(durability)
+  {}
 };
