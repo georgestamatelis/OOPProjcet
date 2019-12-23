f833303ddb393e9c2299b6b3dd91b699a2f799e8
georgestamatelis <52538570+georgestamatelis@users.noreply.github.com>
2019-12-23 16:13:48 +0200

    Add files via upload

--- oopProj/cards/Stronghold.h
+++ oopProj/cards/Stronghold.h
@@ -8,15 +8,11 @@ private:
   int initialMoney;
   int initialDefense;
 public:
-  StrongHold(string name,int c,int b1,int b2,int initialHonor,int initialMoney,int initialDefense)
-  :blackCard(name,c,b1,b2),initialHonor(initialHonor),initialMoney(initialMoney),initialDefense(initialDefense)
+  StrongHold(string name,int c=0,int initialHonor=5,int initialMoney=5,int initialDefense=5)
+  :blackCard(name,c),initialHonor(initialHonor),initialMoney(initialMoney),initialDefense(initialDefense)
   {
 
   }
-/*  StrongHold(string name)
-  :blackCard(name,0,false),
-  {
 
-  }*/
   //StrongHold( int H,int M,int D,)
 };
