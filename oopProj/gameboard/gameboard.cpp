e7eb87617d9b782447dfd498be0d1fe9293e09dd
georgestamatelis <52538570+georgestamatelis@users.noreply.github.com>
2019-12-24 17:57:33 +0200

    Add files via upload

--- oopProj/gameboard/gameboard.cpp
+++ oopProj/gameboard/gameboard.cpp
@@ -3,9 +3,18 @@
 
 gameboard::gameboard(){
 cout<<"Initializing GameBoard"<<endl;
-num_of_players=P1.getNumOfPlayers();
-players=P1.get_Players();
+cout<<"How many players ? :";
+cin>>num_of_players;
+
+players =new Player *[num_of_players];
+for(int i=0;i<num_of_players;i++)
+  players[i]=new Player;
+P1=new phase1(players,num_of_players);
+P3=new phase3(players,num_of_players);
 gamePlay();
+}
+bool isWinner(){
+  return true;
 }
 void gameboard:: printGameStatistics()
 {
@@ -13,7 +22,12 @@ void gameboard:: printGameStatistics()
 }
 void gameboard:: gamePlay()
 {
- cout<<"GamePlay Begins"<<endl;
+  do{
+    cout<<"GamePlay Begins"<<endl;
+    P1->play();
+    P3->play();
+  }while(!isWinner());
+
 }
 gameboard:: ~gameboard(){
   cout<<"Destroying GameBoard game over"<<endl;
--- oopProj/phase1/phase1.h
+++ oopProj/phase1/phase1.h
@@ -6,15 +6,16 @@ class phase1
 {
 private:
   int num_of_players;
-  Player *players;
+  Player **players;
 public:
-  phase1();
+  phase1(Player **pl,int n);
   ~phase1();
+  void play();
   //geters
   int getNumOfPlayers(){
     return num_of_players;
   }
-  Player *get_Players(){
+  Player **get_Players(){
     return players;
   }
 };
