fb926c7ba9d897e479f25ea034ce3306ae642c7c
georgestamatelis <52538570+georgestamatelis@users.noreply.github.com>
2019-12-27 12:47:52 +0200

    Add files via upload

---
+++ oopProject/phase5/phase5.cpp
@@ -0,0 +1,23 @@
+#include "phase5.h"
+
+phase5 ::phase5(Player **players,int num_of_players)
+:players(players),num_of_players(num_of_players)
+{
+
+
+}
+void phase5:: play(){
+  for(int  i=0;i<num_of_players;i++)
+  {
+    players[i]->discardSurplusFateCards();
+  }
+  for(int i=0;i<num_of_players;i++)
+  {
+      players[i]->printHand();
+      players[i]->printProvinces();
+      //players[i]->printHoldings();
+  }
+}
+phase5:: ~phase5(){
+
+}
