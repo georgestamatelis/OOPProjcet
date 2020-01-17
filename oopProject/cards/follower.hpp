20bd5ccaf87a61e43eb1e8858648d24c82ca1e00
georgestamatelis <52538570+georgestamatelis@users.noreply.github.com>
2020-01-17 12:43:39 +0200

    Add files via upload

---
+++ oopProject/cards/follower.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "greencard.hpp"
+
+class follower:public greenCard
+{
+private:
+  std::string type;
+public:
+  follower(string name,int cost,int aB,int dB,int mh,int eb,int ec,string text="follower")
+  :greenCard(name,cost,aB,dB,mh,eb,ec,text)
+  {
+    type="follower";
+  }
+  void print(){
+    cout<<"Follower: "<<getname()<<endl;
+  }
+  std::string GetType(){return type;}
+};
