4de61040d63be99bf7772824d374bc316a2752d7
georgestamatelis <52538570+georgestamatelis@users.noreply.github.com>
2020-02-26 15:40:03 +0200

    Add files via upload

---
+++ Stronghold.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "blackcard.hpp"
+
+class StrongHold : public blackCard{
+private:
+  int initialHonor;
+  int initialMoney;
+  int initialDefense;
+
+public:
+  StrongHold(std::string name, int c = 0, int initialHonor = 5, int initialMoney = 5, int initialDefense = 5)
+      : blackCard(name, c), initialHonor(initialHonor), initialMoney(initialMoney), initialDefense(initialDefense){}
+  int getInitialHonor() { return initialHonor; }
+  int getInitialMoney() { return initialMoney; }
+  int getInitialDefense() { return initialDefense; }
+  void print(){
+    std::cout << "StrongHold name" << this->getname() << std::endl;
+  }
+  //StrongHold( int H,int M,int D,)
+};
